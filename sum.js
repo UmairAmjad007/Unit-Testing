afb27c4975a507f8466f31127c17f6243aeba421
UmairAmjad007 <umairamjad3074@gmail.com>
2022-11-07 13:28:52 +0500

    modified

--- sum.js
+++ sum.js
@@ -3,4 +3,21 @@ function sum(a,b){
 return a+b;
 
 }
-module.exports = sum ;
+function subtract(a,b){
+
+    return a-b;
+    
+ }
+ function multiply(a,b){
+
+    return a*b;
+    
+  }
+  function divide(a,b){
+    return a/b;
+  }
+
+module.exports.divide =  divide ;
+module.exports.multiply = multiply ;
+module.exports.sum= sum ;
+module.exports.subtract= subtract ;
--- sum.test.js
+++ sum.test.js
@@ -3,5 +3,23 @@ const sum =  require('./sum');
 
 test ('add 1 + 2 to equal 3', ()=> {
 
-    expect(sum(1,2)).toBe(3)
+    expect(sum.sum(1,2)).toBe(3)
+});
+
+test ('subtract 2 - 1 to equal 1', ()=> {
+
+    expect(sum.subtract(2,1)).toBe(1)
+});
+test ('multiply 1 * 2 to equal 2', ()=> {
+
+    expect(sum.multiply(1,2)).toBe(2)
+   // expect(sum.multiply(1,2)).toBe(2)
+});
+test ('divide 2 / 2 to equal 1', ()=> {
+
+    expect(sum.divide(2,2)).toBe(1)
+});
+test ('divide 2 / 2 to equal 1', ()=> {
+
+    expect(sum.divide(2,0)).toBe(undefined)
 });
